45c9976c1e2a49475f2b10aba4000e80822a4904
Gayathrigayu <36566787+gayathrikk@users.noreply.github.com>
2024-04-26 12:01:50 +0530

    ShortcutKey_add.java

--- src/test/java/com/gayu/Gayathri/ShortcutKey_add.java
+++ src/test/java/com/gayu/Gayathri/ShortcutKey_add.java
@@ -27,8 +27,7 @@ public class ShortcutKey_add {
 		URL url = new URL("http://172.12.20.99:4444/wd/hub");
 		driver = new RemoteWebDriver(url, dc);
 	}
-
-	@Test(priority = 1)
+@Test(priority = 1)
 	public void Login() throws InterruptedException {
 		driver.get("http://apollo2.humanbrain.in/");
 		driver.manage().window().maximize();
@@ -102,7 +101,7 @@ public class ShortcutKey_add {
 		try {
 			WebDriverWait wait7 = new WebDriverWait(driver, 30);
 			WebElement table2 = wait7.until(ExpectedConditions.elementToBeClickable(By.xpath(
-					"/html/body/ngx-app/ngx-annotation/nb-layout/div/div/div/div/div/nb-layout-column/ngx-series-set-comp/div/ngx-explore-comp/nb-layout/div[1]/div/div/div/div/nb-layout-column[2]/nb-card/nb-card-body/div/div[2]/div/nb-card/nb-card-footer/nb-icon[1]/img")));
+					"/html/body/ngx-app/ngx-annotation/nb-layout/div[1]/div/div/div/div/nb-layout-column/ngx-series-set-comp/div/ngx-explore-comp/nb-layout/div/div/div/div/div/nb-layout-column[2]/nb-card/nb-card-body/div/div[2]/div/nb-card/nb-card-footer/nb-icon[1]/img")));
 			table2.click();
 			System.out.println("--------------------------*****************-----------------------");
 			System.out.println("The Altas Editor is clicked");
